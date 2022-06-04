27bec357b85128f4a08e9196c1bef18382282135
vgtmhl <vgtmhl@gmail.com>
2022-06-04 19:33:31 +0200

    comment

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/**
+ * Whenever the "real" value changes, we start a timer after which we return the debounced value.
+ * If the hook is not called again, the timer will expire and return.
+ * 
+ * If the hook is valled again, then the timer will be re-initialized
+ */
+
 const useDebounce = (value: any, delay: number) => {
     const [debouncedValue, setDebouncedValue] = React.useState(value)
 
